fa819681423aa554efe8c0c4b066adfe6e7efc94
IronForce-Auscent <discordgamer.070910@gmail.com>
2023-12-05 19:12:14 +0800

    Added Day 5 part 2 solution + walkthrough. Credits to u/Biggergig's walkthrough video

--- 2023/Day 5/Part 2/solution.py
+++ 2023/Day 5/Part 2/solution.py
@@ -1,90 +1,143 @@
-import time
-import logging
+class Range:
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
 
-class Part1():
+    def __repr__(self) -> str:
+        return f"[{self.lower}, {self.upper}]"
+    
+    def intersection(self, other):
+        """
+        Returns the highest minimum and smallest maximum intersection points of 2 ranges as a Range() object, or None if the ranges do not intersect
+
+        Example:
+        ----------
+                -------
+        Code returns Range(8, 10)
+
+        ---------
+                    -----
+        Code returns None
+        """
+        tmp = Range(max(self.lower, other.lower), min(self.upper, other.upper))
+        return tmp if tmp.lower < tmp.upper else None
+
+    def subtraction(self, other) -> list:
+        """
+        Returns the subtraction of 2 intersecting ranges
+        """
+        intersection = self.intersection(other)
+        if intersection == None:
+            """
+            If there are no intersections found, return Range() object containing the current lower and upper values
+
+            Visualisation:
+            --------          (Our range)
+                     -------- (Intersecting range)
+            """
+            return [Range(self.lower, self.upper)]
+        elif (intersection.lower, intersection.upper) == (self.lower, self.upper):
+            """
+            If the lower and upper values of the intersecting range is the same as ours, return an empty list
+
+            Visualisation:
+            -------- (Our range)
+            -------- (Intersecting range)
+            """
+            return []
+        elif intersection.lower == self.lower:
+            """
+            If the lower value of the intersecting range is the same as ours, return a Range() object with the upper values of the two ranges
+
+            Visualisation:
+            --------- (Our range)
+            ---       (Intersecting range)
+            """
+            return [Range(intersection.upper, self.upper)]
+        elif intersection.upper == self.upper:
+            """
+            If the upper value of the intersecting range is the same as ours, return a Range() object with the lower values of the two ranges
+
+            Visualisation:
+            ------- (Our range)
+               ---- (Intersecting range)
+            """
+            return [Range(intersection.lower, self.lower)]
+        else:
+            """
+            If none of the ranges' values match, return a list of two Range() objects containing the lower and upper values respectively
+
+            Visualisation:
+            --------- (Our range)
+              ---     (Intersecting range)
+            """
+            return [Range(intersection.lower, self.lower), Range(intersection.upper, self.upper)]
+    
+    def add(self, offset: int | float):
+        return Range(self.lower + offset, self.upper + offset)
+
+class Map:
+    def __init__(self, map_str):
+        self.rules = []
+        for line in map_str.splitlines()[1:]:
+            # line = "50 98 2"
+            # line.split() = ["50", "98", "2"]
+            dest, src, size = map(int, line.split())
+            self.rules.append((dest, src, size))
+    
+    def convert(self, _input):
+        for dest, src, size in self.rules:
+            if src <= _input < (src + size):
+                return dest + _input - src
+            
+        return _input
+
+class Part2():
     def __init__(self):
-        self.almanac = {}
-        self.seeds: list[int] = []
-        self.mappings: dict[list[list[int], list[int]]] = {}
-        self.categories = ["seeds", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
-        logging.basicConfig(filename="logger.log", encoding="utf-8", level=logging.DEBUG, filemode="w")
+        self.maps = None
+        self.answer = float('inf')
 
     def read_data(self):
-        with open("input.txt", "r") as f:
-            data = f.read().split("\n\n")
-        return data
-
-    def populate_almanac(self, data):
-        logging.info(f"Populating alamanc...")
-        self.almanac["seeds"] = data[0].split(": ")[1].split(" ")
-        self.almanac["soil"] = data[1].split("\n")[1:]
-        self.almanac["fertilizer"] = data[2].split("\n")[1:]
-        self.almanac["water"] = data[3].split("\n")[1:]
-        self.almanac["light"] = data[4].split("\n")[1:]
-        self.almanac["temperature"] = data[5].split("\n")[1:]
-        self.almanac["humidity"] = data[6].split("\n")[1:]
-        self.almanac["location"] = data[7].split("\n")[1:]
-        logging.info("Almanac populated")
-    
-    def check_if_within_range(self, src: int, dest: int, length: int, current: int):
-        src, dest, length, current = int(src), int(dest), int(length), int(current)
-        if src <= current < (src + length):
-            return dest + (current - src)
-        else:
-            return False
-
-    def calculate_location(self, seed: int, key: str):
-        flag = False
-        value = self.almanac[key]
-        response = 0
-        for entry in value:
-            dest, src, length = entry.split()
-            response = self.check_if_within_range(src, dest, length, seed)
-            if response is not False:                
-                flag = True
-                break
+        with open("sample.txt", "r") as f:
+            return f.read().split("\n\n")
         
-        if not flag:
-            logging.error(f"Seed not mapped, returning original value")
-            response = seed
-            logging.info(f"New response value: {response}")
+    def propagate(self, r: Range, layer: int):
+        """
+        Goes through each layer ("map") and process them until it reaches the final layer ("locations")
+        """
+        if layer == len(self.maps):
+            self.answer = min(self.answer, r.lower)
+            return
         
-        if key == "location":
-            return response
-        else:
-            next_category = self.categories[self.categories.index(key) + 1]
-            logging.debug(f"Current category: {key}; Category index: {self.categories.index(key)}")
-            logging.debug(f"Next category: {next_category}; Category index: {self.categories.index(key) + 1}")
-            return self.calculate_location(response, next_category)
-    
-    def parse_seed_data(self):
-        seed_bank = self.almanac["seeds"]
-        for i in range(0, len(seed_bank), 2):
-            seed_id, length = int(seed_bank[i]), int(seed_bank[i+1])
-            for d in range(length):
-                self.seeds.append(seed_id + d)
+        for dest, src, size in self.maps[layer].rules:
+            map_range = Range(src, src + size)
+            intersection = r.intersection(map_range)
+            if intersection is not None:
+                self.propagate(intersection.add(dest - src), layer + 1)
+                subtraction = r.subtraction(map_range)
+                if len(subtraction) == 0:
+                    # All the rules have been handled
+                    return
+                r = subtraction[0]
+                if len(subtraction) == 2:
+                    self.propagate(subtraction[1], layer)
+        self.propagate(r, layer + 1)
+
+    def get_location(self, seed, maps):
+        for _map in maps:
+            seed = _map.convert(seed)
+        return seed
 
     def main(self):
-        data = self.read_data()
-        self.populate_almanac(data)
-        print(self.almanac["seeds"])
-        self.parse_seed_data()
+        seeds, *map_strs = self.read_data()
+        seeds = list(map(int, seeds.split()[1:]))
+        self.maps = [Map(map_str) for map_str in map_strs]
 
-        smallest = -1
-        for seed in self.seeds:
-            result = self.calculate_location(seed, "soil")
-            if smallest == -1 or result < smallest:
-                smallest = result
-
-        logging.info("Execution complete")
-        logging.info(f"Smallest location ID: {smallest}")
-
+        for i in range(0, len(seeds), 2):
+            self.propagate(Range(seeds[i], seeds[i] + seeds[i + 1]), 0)
+        print(self.answer)
+        return None
 
 if __name__ == "__main__":
-
-    part1 = Part1()
-    start = time.time()
-    part1.main()
-    end = time.time()
-    delta = end - start
-    print(f"Total execution time: {delta} seconds")
+    part2 = Part2()
+    part2.main()
