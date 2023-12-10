61e7f391c7875e434ddfa53d9029bcc146f36e73
IronForce-Auscent <discordgamer.070910@gmail.com>
2023-12-10 12:24:45 +0800

    Updated .gitignore to exclude input data, previous input data will be left in the repository as there doesn't seem to be an easy way to remove them

---
+++ 2023/Day 10/Part 1/solution.py
@@ -0,0 +1,15 @@
+class Part1():
+    def __init__(self):
+        pass
+
+    def read_data(self):
+        with open("sample.txt", "r") as f:
+            document = f.read()
+        return document
+
+    def main(self):
+        data = self.read_data()
+
+if __name__ == "__main__":
+    part1 = Part1()
+    part1.main()
